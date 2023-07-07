304a6ea1c73f0ec37a122b8143d44f6cb831dd3f
avinella <avinella@g.hmc.edu>
2023-07-07 10:37:46 -0700

    improve debug drawing

--- include/debug_drawer.hpp
+++ include/debug_drawer.hpp
@@ -5,75 +5,28 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include<vector>
 #include "shader.hpp"
 
 class DebugDrawer : public btIDebugDraw {
 public:
     GLuint VBO, VAO;
-    DebugDrawer()
-        : m{0}, shader{0,0,"assets/gl/debug.vs","assets/gl/debug.fs"}
-    {
-    }
+    DebugDrawer();
 
-	void SetMatrices(glm::mat4 viewMatrix, glm::mat4 projectionMatrix) 
-	{
-        shader.use();
-        shader.setMat4("projection", projectionMatrix);
-        shader.setMat4("view", viewMatrix);
-	}
+	void SetMatrices(glm::mat4 viewMatrix, glm::mat4 projectionMatrix);
 
-	virtual void drawLine(const btVector3& from, const btVector3& to, const btVector3& color) 
-	{
-		// Vertex data
-		GLfloat points[12];
+	virtual void drawLine(const btVector3& from, const btVector3& to, const btVector3& color); 
 
-		points[0] = from.x();
-		points[1] = from.y();
-		points[2] = from.z();
-		points[3] = color.x();
-		points[4] = color.y();
-		points[5] = color.z();
-
-		points[6] = to.x();
-		points[7] = to.y();
-		points[8] = to.z();
-		points[9] = color.x();
-		points[10] = color.y();
-		points[11] = color.z();
-
-        shader.use();
-
-
-		glDeleteBuffers(1, &VBO);
-		glDeleteVertexArrays(1, &VAO);
-		glGenBuffers(1, &VBO);
-		glGenVertexArrays(1, &VAO);
-		glBindVertexArray(VAO);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(points), &points, GL_STATIC_DRAW);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*) 0);
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), 
-            (void*)(3 * sizeof(GLfloat)));
-		glBindVertexArray(0);
-
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_LINES, 0, 2);
-		glBindVertexArray(0);
-
-	}
-	virtual void drawContactPoint(const btVector3 &, const btVector3 &, btScalar, int, const btVector3 &) {}
-	virtual void reportErrorWarning(const char * warn) 
-    {
-        std::cout << warn << std::endl;
-    }
-	virtual void draw3dText(const btVector3 &, const char *) {}
-	virtual void setDebugMode(int p) {
-		m = p;
-	}
-	int getDebugMode(void) const { return m; }
+	virtual void drawContactPoint(const btVector3 &, const btVector3 &, btScalar, int, const btVector3 &);
+	
+    virtual void reportErrorWarning(const char * warn);
+	virtual void draw3dText(const btVector3 &, const char *);
+	virtual void setDebugMode(int p);
+	int getDebugMode(void) const;
+    void drawLinesWorld();
 	int m;
     Shader shader;
+    std::vector<GLfloat> points_;
+    int numPoints_;
 };
 #endif
---
+++ src/debug_drawer.cpp
@@ -0,0 +1,77 @@
+#include "debug_drawer.hpp"
+
+DebugDrawer::DebugDrawer()
+    : m{0}, shader{0,0,"assets/gl/debug.vs","assets/gl/debug.fs"}
+{
+}
+
+void DebugDrawer::SetMatrices(glm::mat4 viewMatrix, glm::mat4 projectionMatrix) 
+{
+    shader.use();
+    shader.setMat4("projection", projectionMatrix);
+    shader.setMat4("view", viewMatrix);
+}
+
+void DebugDrawer::drawLine(const btVector3& from, const btVector3& to, const btVector3& color)
+{
+    points_.push_back(from.x());
+    points_.push_back(from.y());
+    points_.push_back(from.z());
+    points_.push_back(color.x());
+    points_.push_back(color.y());
+    points_.push_back(color.z());
+    points_.push_back(to.x());
+    points_.push_back(to.y());
+    points_.push_back(to.z());
+    points_.push_back(color.x());
+    points_.push_back(color.y());
+    points_.push_back(color.z());
+    numPoints_ += 2;
+}
+
+void DebugDrawer::drawContactPoint(const btVector3 &, const btVector3 &, btScalar, int, const btVector3 &) {}
+
+void DebugDrawer::reportErrorWarning(const char * warn) 
+{
+    std::cout << warn << std::endl;
+}
+
+void DebugDrawer::draw3dText(const btVector3 &, const char *) {}
+
+void DebugDrawer::setDebugMode(int p) { m = p; }
+
+int DebugDrawer::getDebugMode(void) const { return m; }
+
+void DebugDrawer::drawLinesWorld() {
+    shader.use();
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, points_.size() * sizeof(GLfloat), &points_[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        6*sizeof(GLfloat),
+        (void*) 0
+    );
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        6*sizeof(GLfloat),
+        (void*) (3 * sizeof(GLfloat))
+    );
+    glBindVertexArray(0);
+
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_LINES, 0, numPoints_);
+    glBindVertexArray(0);
+    points_.clear();
+    numPoints_ = 0;
+}
